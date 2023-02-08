dc617259328df2a27b6b7c889f9026b74f42b078
darlingdedeepya <87213449+darlingdedeepya@users.noreply.github.com>
2023-02-07 16:39:43 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ function setup(){
     canvas.center();
     background("white");
     synth=window.speechSynthesis;
-    //canvas.mouseReleased(classifyCanvas); 
+    canvas.mouseReleased(classifyCanvas); 
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
 }
 
 function clearCanvas(){
@@ -11,9 +15,26 @@ function clearCanvas(){
 }
 
 function draw(){
-    strokeWeight(10);
+    strokeWeight(13);
     stroke(0);
     if (mouseIsPressed) {
         line(pmouseX, pmouseY, mouseX, mouseY);
     }
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'label:' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence:' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
